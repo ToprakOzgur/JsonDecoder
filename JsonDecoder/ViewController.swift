ecc9e7ae9ebd84d245363d788300f0afb509a91b
ToprakOzgur <zgrtprk@gmail.com>
2018-02-14 17:03:10 +0300

    Swift 2-3 -ObjC Type Jason Parse

--- JsonDecoder/ViewController.swift
+++ JsonDecoder/ViewController.swift
@@ -4,22 +4,55 @@
 //
 //  Created by Ozgur Toprak on 14.02.2018.
 //  Copyright © 2018 Ozgur Toprak. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Course {
+    let id: Int
+    let name: String
+    let link: String
+    let imageURL: String
+    
+    init(json: [String: Any]) {
+        id = json["id"] as? Int ?? -1
+        name = json["name"] as? String ?? ""
+        link = json["link"] as? String ?? ""
+        imageURL = json["imageURL"] as? String ?? ""
+    }
+}
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        guard  let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else {return}
+            
+            //            let dataAsString = String(data: data, encoding: .utf8)
+            //            print(dataAsString)
+            
+            do {
+                guard let json  = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] else { return }
+                
+                let course = Course(json: json)
+                print(course.name
+                )
+                
+            } catch let jsonnErr {
+                print("Error seriaalizin json:",jsonnErr)
+            }
+            
+            }.resume()
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
 }
 
